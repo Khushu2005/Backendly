be6ee3c11f6a5c68d2bcb9b110ffb8dd1f7fe8a5
Khushi sharma <163833907+Khushu2005@users.noreply.github.com>
2025-09-26 21:23:44 +0530

    notes application done

--- notes-application/Server.js
+++ notes-application/Server.js
@@ -4,11 +4,48 @@ let server = express();
 server.use(express.json());
 
 let notes = [];
+
+// msg
+server.get('/', (req, res) => {
+    res.send('Welcome to the Notes Application');
+})
+
+// add note
 server.post('/notes', (req, res) => {
     notes.push(req.body);
     res.json({
         message: 'Note added successfully',
-        notes: notes
+
+    })
+
+})
+
+// get all notes
+server.get('/notes', (req, res) => {
+    res.json(notes);
+})
+
+// delete note
+server.delete('/notes/:index', (req, res) => {
+    let index = req.params.index;
+    delete notes[index];
+
+    res.json({
+        message: 'Note deleted successfully'
+    })
+})
+
+// update note
+server.patch('/notes/:index', (req, res) => {
+    let index = req.params.index;
+    let title = req.body.title;
+    let description = req.body.description;
+
+    notes[index].title = title;
+    notes[index].description = description;
+
+    res.json({
+        message: 'Note updated successfully'
     })
 
 })
